e6ce4f56fdc78b835e0a5e44c26f1b241dadb841
Abdul R. Arraisi <itsarraisi@gmail.com>
2021-01-23 05:06:18 +0700

    Initial Commit

---
+++ ActivityIndicator SwiftUI/ActivityIndicator_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActivityIndicator_SwiftUIApp.swift
+//  ActivityIndicator SwiftUI
+//
+//  Created by Abdul R. Arraisi on 23/01/21.
+//
+
+import SwiftUI
+
+@main
+struct ActivityIndicator_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ActivityIndicator SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ActivityIndicator SwiftUI
+//
+//  Created by Abdul R. Arraisi on 23/01/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
