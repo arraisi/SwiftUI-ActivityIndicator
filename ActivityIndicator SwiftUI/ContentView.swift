7e0c22291159c666a1e32f940e6e0c0533733f0b
Abdul R. Arraisi <itsarraisi@gmail.com>
2021-01-24 17:59:10 +0700

    first commit

--- ActivityIndicator SwiftUI/ContentView.swift
+++ ActivityIndicator SwiftUI/ContentView.swift
@@ -8,14 +8,51 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var startAnimating = true
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(spacing: 100) {
+            ZStack {
+                ActivityIndicator(startAnimating: $startAnimating)
+                
+                if !startAnimating {
+                    Text("Hello, world!")
+                }
+            }
+            
+            Button(action: {
+                startAnimating.toggle()
+            }, label: {
+                Text("Start / Stop")
+                    .padding()
+            })
+            .foregroundColor(.white)
+            .background(Color.blue)
+            .cornerRadius(15)
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct ActivityIndicator: UIViewRepresentable {
+    
+    @Binding var startAnimating: Bool
+    
+    func makeUIView(context: Context) -> UIActivityIndicatorView {
+        return UIActivityIndicatorView()
+    }
+    
+    func updateUIView(_ uiView: UIActivityIndicatorView, context: Context) {
+        if startAnimating {
+            uiView.startAnimating()
+        } else {
+            uiView.stopAnimating()
+        }
     }
 }
